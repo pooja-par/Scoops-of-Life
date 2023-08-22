407e886acdbc0e4f2c86f7879df48b311726032d
pooja-par <>
2023-08-22 21:42:15 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Code is prepared to help icecream seller to find optimize the icecream sales.
+Shop has the following icecream flavors. 
+1. vanila, 2. browncrunch, 3. saffron, 4. caramel apple 5. mocha macchiato
+6. peanutbutter pie 7. dark chocolate 8. strawberry 9. lemon
+"""
 import gspread
 from google.oauth2.service_account import Credentials
-import math  # Import the math module
+import math  # Import the math library
 
 
 SCOPE = [
@@ -22,36 +25,45 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
+    via the terminal, which must be a string of 9 numbers separated
+    by commas. These numbers represent scoop of icecreams for the different flavors.
+    Icecreams are normally measured in Kg to compare with the stock. 1 scoop is assumed to be 0.1 kg. 
+    Scoops are being converted in kg by multiplication of each scoop with 0.1. 
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+        print("Example: 10,20,30,40,50,60,70,80,90\n")
+        print("In the sequence of:")
+        print("vanila, browncrunch, saffron, caramel apple, mocha macchiato, peanutbutter pie, dark chocolate, strawberry, lemon")
 
         data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
+        
 
         if validate_data(sales_data):
             print("Data is valid!")
             break
+
+        sales_data = sales_data*0.1         # convert scoop into kg. 
 
     return sales_data
 
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    Inside the try, converts all string values into float.
+    Raises ValueError if strings cannot be converted into float,
+    or if there aren't exactly 9 values.
     """
     try:
-        [int(value) for value in values]
-        if len(values) != 6:
+        for value in values:
+            value = float(value)
+         
+        if len(values) != 9:
             raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
+                f"Exactly 9 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
@@ -135,13 +147,14 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
+    """
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-
+    """
     
 
 
